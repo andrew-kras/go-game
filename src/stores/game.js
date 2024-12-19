da6eed798f7b581457952d39fcc684379b537065
andrew-kras <krasilnikov-ad@m13.ru>
2024-12-19 23:13:37 +0300

    WIP: simple game board added

--- src/stores/game.js
+++ src/stores/game.js
@@ -1,21 +1,45 @@
 import { defineStore } from 'pinia'
 
 export const GETTER_SIZE = 'getterSize'
+export const GETTER_BOARD = 'getterBoard'
+export const GETTER_CURRENT_PLAYER = 'getterCurrentPlayer'
 
 export const ACTION_SELECT_BOARD_SIZE = 'actionSelectBoardSize'
+export const ACTION_MAKE_MOVE = 'actionMakeMove'
+export const ACTION_RESET_GAME = 'actionResetGame'
 
 export default defineStore('game', {
     state: () => ({
-        size: ''
+        size: '',
+        board: [],
+        currentPlayer: 'black'
     }),
 
     getters: {
-        [GETTER_SIZE]: state => state.size
+        [GETTER_SIZE]: state => state.size,
+        [GETTER_BOARD]: (state) => state.board,
+        [GETTER_CURRENT_PLAYER]: (state) => state.currentPlayer
     },
 
     actions: {
         async [ACTION_SELECT_BOARD_SIZE](value) {
             this.size = value
+        },
+
+        [ACTION_RESET_GAME]() {
+            const size = this.size === 'small' ? 9 : this.size === 'medium' ? 13 : 19
+            this.board = Array.from({ length: size }, (_, row) =>
+                Array.from({ length: size }, (_, col) => ({ id: `${row}-${col}`, value: null }))
+            )
+            this.currentPlayer = 'black'
+        },
+
+        [ACTION_MAKE_MOVE](cellId) {
+            const [row, col] = cellId.split('-').map(Number)
+            if (!this.board[row][col].value) {
+                this.board[row][col].value = this.currentPlayer
+                this.currentPlayer = this.currentPlayer === 'black' ? 'white' : 'black'
+            }
         }
     }
 })
--- src/views/gameView.vue
+++ src/views/gameView.vue
@@ -1,19 +1,76 @@
 <template lang="pug">
   .game-view
     .title {{ $t('title') }}
-    .game-board
+    g-button(
+      :text='$t("reset")'
+      @click='resetGame'
+    )
+    .game-board(:style='{ gridTemplateColumns: `repeat(${boardSize}, 1fr)` }')
+      div.game-cell(
+        v-for='cell in flatBoard'
+        :key='cell.id'
+        :class='{"black-stone": cell.value === "black", "white-stone": cell.value === "white"}'
+        @click='makeMove(cell.id)'
+      )
 </template>
 
 <i18n>
 ru:
   title: Игровое поле
+  reset: Сбросить игру
 en:
   title: Game Board
+  reset: Reset Game
 </i18n>
 
 <script>
+import { mapStores } from 'pinia'
+import gameStore, {
+  GETTER_BOARD,
+  GETTER_CURRENT_PLAYER,
+  ACTION_MAKE_MOVE,
+  ACTION_RESET_GAME
+} from '@/stores/game.js'
+import GButton from '@/components/inputs/GButton.vue'
+
 export default {
-  name: 'gameView'
+  name: 'gameView',
+
+  components: {GButton},
+
+  computed: {
+    ...mapStores(gameStore),
+
+    board() {
+      return this.gameStore[GETTER_BOARD]
+    },
+
+    flatBoard() {
+      return this.board.flat()
+    },
+
+    boardSize() {
+      return this.board.length
+    },
+
+    currentPlayer() {
+      return this.gameStore[GETTER_CURRENT_PLAYER]
+    }
+  },
+
+  methods: {
+    makeMove(cellId) {
+      this.gameStore[ACTION_MAKE_MOVE](cellId)
+    },
+
+    resetGame() {
+      this.gameStore[ACTION_RESET_GAME]()
+    }
+  },
+
+  created() {
+    this.resetGame()
+  }
 }
 </script>
 
@@ -28,9 +85,30 @@ export default {
   .title
     $text-bold()
 
-  .game-board
-    width 80%
-    height 80%
-    border 2px solid #000
-    $flex(jc: center, ai: center)
+.game-board
+  display grid
+  gap 1px
+  background-color $color5
+  grid-template-columns repeat(19, 1fr)
+
+  width 460px
+  height 460px
+
+  border 2px solid $color5
+  border-radius 2px
+
+.game-cell
+  background-color $color4
+  border 1px solid $color5
+  $flex(ai: center, jc: center)
+
+.black-stone
+  background-color $black
+  border-radius 50%
+  $square(50%)
+
+.white-stone
+  background-color $white
+  border-radius 50%
+  $square(50%)
 </style>
