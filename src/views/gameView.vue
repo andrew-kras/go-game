575b67bd859c2d684beba3554d26c83bdf4026ac
andrew-kras <krasilnikov-ad@m13.ru>
2024-12-21 00:46:16 +0300

    WIP: board fixed

--- src/views/gameView.vue
+++ src/views/gameView.vue
@@ -1,13 +1,16 @@
 <template lang="pug">
   .game-view
     .title {{ $t('title') }}
-    .game-board(:style='{ gridTemplateColumns: `repeat(${boardSize}, 1fr)` }')
-      div.game-cell(
-        v-for='cell in flatBoard'
-        :key='cell.id'
-        :class='{"black-stone": cell.value === "black", "white-stone": cell.value === "white"}'
-        @click='makeMove(cell.id)'
-      )
+    .game
+      .game-back-broad(:style='{ gridTemplateColumns: `repeat(${boardSize - 1}, 1fr)` }')
+        .game-cell(v-for='n in backBoardSize' :key='n')
+      .game-board(:style='{ gridTemplateColumns: `repeat(${boardSize}, 1fr)` }')
+        .game-cell(
+          v-for='cell in flatBoard'
+          :key='cell.id'
+          :class='{"black-stone": cell.value === "black", "white-stone": cell.value === "white"}'
+          @click='makeMove(cell.id)'
+        )
     .buttons
       g-button(
         :text='$t("reset")'
@@ -71,6 +74,10 @@ export default {
 
     currentPlayer() {
       return this.gameStore[GETTER_CURRENT_PLAYER]
+    },
+
+    backBoardSize() {
+      return (this.boardSize - 1) * (this.boardSize - 1)
     }
   },
 
@@ -113,20 +120,38 @@ export default {
     $flex(ai: center, jc: center)
     gap 12px
 
-.game-board
-  display grid
-  gap 1px
-  background-color $color5
-  grid-template-columns repeat(19, 1fr)
+  .game
+    width 475px
+    height 475px
 
-  width 460px
-  height 460px
+    .game-back-broad
+      transform translate(15px, 15px)
 
-  border 2px solid $color5
-  border-radius 2px
+      display grid
+      background-color $color4
+      grid-template-columns repeat(18, 1fr)
+      position absolute
+      width 450px
+      height 450px
+      border 1px solid $color5
+      border-radius 4px
+
+      .game-cell
+        $square(100%)
+        border 1px solid $color5
+        background-color $color4
+        $flex(ai: center, jc: center)
+
+    .game-board
+      display grid
+      background-color transparent
+      grid-template-columns repeat(19, 1fr)
+      position relative
+      width 100%
+      height 100%
 
 .game-cell
-  background-color $color4
+  background-color transparent
   $flex(ai: center, jc: center)
 
 .black-stone
